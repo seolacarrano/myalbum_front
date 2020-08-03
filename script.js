46c0da043ea3e051824efd0ea40f83c0b1cfb00a
seolacarrano <seola6@gmail.com>
2020-08-03 18:03:05 -0400

    connecting database

--- script.js
+++ script.js
@@ -1,55 +1,37 @@
-/*const getImages = async () => {
+const $ul = $('ul')
+
+const getImages = async () => {
     const response = await fetch('http://localhost:3000/image')
     const images = await response.json()
     console.log(images)
+    showImages(images)
 }
 
-const showPizzas = 
-getImages()
+const showImages = (images) => {
+    images.forEach(image => {
+        if (!image.url) return
+    
+        const imageNode = document.createElement('img')
+        imageNode.setAttribute('src', image.url)
+        imageNode.classList.add('image')
+    
+        imageNode.addEventListener('click', () => {editModal(image) })
+    
+        images.appendChild(imageNode)
+      })
+}
 
-//set url so it uses deployed api 
-const deployedURL = "https://seolasecondproject.herokuapp.com"
-const URL = deployedURL ? deployedURL : "http://localhost:3000"*/
-
-//variable
-const submit = document.getElementById('submit')
-const editSubmit = document.getElementById('submit-edit')
-const deleteButton = document.getElementById('delete')
-
-// Stores which item to edit/delete when the edit modal is up
-let currentlyEditing = ''
-
-function addPictures (gifData) {
-    // Adds all of the gifs to the dom
-    gifs.innerHTML = ''
-    gifData.forEach(image => {
-      if (!image.url) return
-  
-      const imageNode = document.createElement('img')
-      imageNode.setAttribute('src', image.url)
-      imageNode.classList.add('image')
-  
-      imageNode.addEventListener('click', () => { editModal(image) })
-  
-      gifs.appendChild(imageNode)
-    })
-  }
-
-  fetch('localhost:3000/image')
-  .then(resp=> resp.json())
-  .then(reesp=> {addPictures(res)})
-
-  $('.modal').modal()
-
-
-
-
-
-
-
-
+submit.addEventListener('click', (e) => {
+    // submits the post request to create a new picture
+    const name = document.getElementById('name').value
+    const url = document.getElementById('url').value
 
 /*
+//set url so it uses deployed api 
+//const deployedURL = "https://seolasecondproject.herokuapp.com"
+//const URL = deployedURL ? deployedURL : "http://localhost:3000"
+
+
 //global variables
 const $nameInput = $("#createinput");
 const $imageSelect = $("#createselect");
@@ -134,3 +116,74 @@ const createRat = async (event) => {
   //initially get existing rats*/
 
   
+
+
+
+
+
+
+
+
+
+
+
+/*
+//variable
+const images = document.querySelector('.images')
+const submit = document.getElementById('submit')
+const editSubmit = document.getElementById('submit-edit')
+const deleteButton = document.getElementById('delete')
+
+
+
+function addPictures (imageData) {
+    // Adds all of the gifs to the dom
+    images.innerHTML = ''
+    imageData.forEach(image => {
+      if (!image.url) return
+  
+      const imageNode = $('<img>')
+      imageNode.attr('src', image.url)
+      imageNode.addClass('image')
+  
+      imageNode.on('click', () => { editModal(image) })
+  
+      gifs.append(imageNode)
+    })
+  }
+
+  fetch('http://localhost:3000/image')
+  .then(resp=> resp.json())
+  .then(resp=> {addPictures(resp)})
+
+
+  submit.addEventListener('click', (e) => {
+    // submits the post request to create a new picture
+    const name = $('#name').val()
+    const url = $('#url').val()
+  
+    fetch('http://localhost:3000/image', 
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, url })
+      })
+      .then(resp => resp.json())
+      .then(resp => {
+        addPictures(resp)
+        $('#modal-create').modal('close')
+      })
+  })
+  
+
+  //$('.modal').modal()
+*/
+
+
+
+
+
+
+
