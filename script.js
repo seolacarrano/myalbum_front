e7ccad3c08c98d1f3d4709afb5acc0e57fa922de
seolacarrano <seola6@gmail.com>
2020-08-03 17:04:34 -0400

    connecting database

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-const getImages = async () => {
+/*const getImages = async () => {
     const response = await fetch('http://localhost:3000/image')
     const images = await response.json()
     console.log(images)
 }
 
 const showPizzas = 
-getImages()
+getImages()*/
+
+//set url so it uses deployed api 
+const deployedURL = "https://seolasecondproject.herokuapp.com"
+const URL = deployedRUL ? deployedURL : "http://localhost:3000"
+
+//global variables
+const $nameInput = $("#createinput")
+
+//get image from api and populate selector input
+const getImage = async () => {
+    const response = await fetch(`${URL}/image`);
+    const data = await response.json();
+    data.forEach((image) => {
+        const $option = $("<option>").attr("value", image._id).text(image.title);
+        $imageSelect.append($option);
+        const $option2 = $("<option>").attr("value", image._id).text(image.title);
+        $imageEditSelect.append($option2);
+    })
+}
