b166f178c7fbe7f2cedc2836a495015c6aa953be
seolacarrano <seola6@gmail.com>
2020-08-03 23:13:47 -0400

    creating image

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const $ul = $("ul");
 const $button = $("#createbutton");
 const $images = $(".images")
-
+const $submit = $('#submit')
 
 //get images from api and populate selector input
 const getImage = async () => {
@@ -12,32 +12,73 @@ const getImage = async () => {
     data.forEach((image) => {
       const imageNode = document.createElement('img')
       imageNode.setAttribute('src', image.url)
-      imageNode.classList.add('image')
+      //imageNode.classList.add('image')
       
       $images.append(imageNode)
     })
 };
 
 
-//create a new image
-const createImage = async (event) => {
-    //Create to New Image from Form Data
-    const newImage = {
-      name: $nameInput.val(),
-      url: $urlInput.val(),
-    };
+//add a new image
+const addImage = async (imageData) => {
+  //$images.empty()
+  imageData.forEach((image) => {
+    if (!image.url) return
+
+    const imageNode = $('<img>')
+    imageNode.attr('src', image.url)
+    //imageNode.addClass('image') for css
+
+    //imageNode.on('click', () => { editModal(gif) }) for edit 
+
+    $images.append(imageNode)
+  })
+}
+
+$submit.on('click', (e) => {
+    // submits the post request to create a new picture
+    const name = $('#name').val()
+    const url = $('#url').val()
+  
+    fetch('http://localhost:3000/image', 
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, url })
+      })
+      .then(resp => resp.json())
+      .then(resp => {
+        addImage(resp)
+        
+      })
+      $images.empty()
+        getImage()
+  })
   
     //Send request to api to create an image
-    const response = await fetch(`${URL}/image`)
-    .then(resp=> resp.json())
-    .then(resp=> {addPictures(resp)})
-    const data = response.json();
-
-    $images.empty()
-    getImage()
-
-};
-  
+    /*const response = await fetch(`http://localhost:3000/image`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(createImage),
+      })
+      const data = response.json();
+    
+      //update the DOM
+      $images.empty()
+      getImage()
+    };*/
+ fetch('http://localhost:3000/image')
+  .then(resp => resp.json())
+  .then(resp => {
+    // gets the initial data
+    addImage(resp)
+  })
+    
+  /*
 //delete image
 const deleteImage = async (event) => {
     const response = await fetch(`http://localhost:3000/gifs${event.target.id}`, {
@@ -47,7 +88,7 @@ const deleteImage = async (event) => {
       //update the dom
       $ul.empty()
       getImage() 
-}
+}*/
 
-getImage()
-$button.on('click', createImage)
+//getImage()
+//$button.on('click', addImage)
